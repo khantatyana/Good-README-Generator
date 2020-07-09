7a7618feb6f4aada4096110e57871673d2c2630b
Tatyana <hanyatan@gmail.com>
2020-07-08 21:40:27 -0400

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # Good-README-Generator
+
+        [![Build Status](https://img.shields.io/badge/<Project-title>-<Good-README-Generator>-<red>)](https://img.shields.io/badge/<Project-title>-<Good-README-Generator>-<red>)
+        
+        ## Description
+
+        Create a command-line application that dynamically generates a README.md from a user's input. The application will be invoked with Using Node.js using ES6+. The application will be invoked with the following command:
+                
+        '''sh
+        node index.js
+        '''
+        
+        ## Contents
+        Project title, Description, Installation, Usage, License, Contributing 
+        
+        ## Installation 
+
+        npm installation
+        
+        ## Author
+        
+        Tatyana Khan
+        
+        ## Project homepage
+        
+        [GitHub page](https://github.com/khantatyana/Good-README-Generator)
+        
+        ## Usage
+        
+        The user is prompted the GitHub username and other information pertaining to the project the README is for. Minimum requirement for this usage is npm installation.
+        
+        ## License name
+        
+        MIT
+        
+        ## License URL
+        
+        https://github.com/khantatyana/Good-README-Generator/blob/master/LICENSE
+        
+        ## Contributing
+        
+        1. Fork it!
+        
+        ## Tests
+        
+        5. Submit a pull request This particular repo doesn't require testing.
+        
+        ## GitHub username
+
+        [GitHub profile](https://github.com/khantatyana)
+        
+        ## User GitHub profile picture
+        
+        ![Picture](https://github.com/khantatyana)
+        
+        ## User GitHub email
+
+        
+        
+        ## User story:
+        
+        
+        AS A developer
+        
+        I WANT a README generator
+        
+        SO THAT I can easily put together a good README for a new project
+        
---
+++ index.js
@@ -0,0 +1,171 @@
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+userPrompts();
+async function userPrompts() {
+    try {
+        const userInput = await inquirer.prompt([
+            {
+                type: "input",
+                message: "Please enter a badge link",
+                name: "badge"
+            },
+            {
+                type: "input",
+                message: "Please enter your project title",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Please enter Description",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "Please enter Table of Contents",
+                name: "contents"
+            },
+            {
+                type: "input",
+                message: "What is Installation for this Project?",
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: "Author",
+                name: "author"
+            },
+            {
+                type: "input",
+                message: "Please enter Project homepage",
+                name: "homepage"
+            },
+            {
+                type: "input",
+                message: "Usage",
+                name: "usage"
+            },
+            {
+                type: "list",
+                message: "Please enter License name",
+                name: "licenseName",
+                choices: [
+                    "MIT",
+                    "Apache license 2.0",
+                    "ISC",
+                    "Mozilla Public License 2.0"
+                ]
+            },
+            {
+                type: "input",
+                message: "Please enter License URL",
+                name: "licenseURL"
+            },
+            {
+                type: "input",
+                message: "Contributing",
+                name: "contributing"
+            },
+            {
+                type: "input",
+                message: "Tests",
+                name: "tests"
+            },
+            {
+                type: "input",
+                message: "What is your GitHub username?",
+                name: "username"
+            },
+            {
+                type: "input",
+                message: "User GitHub profile picture",
+                name: "picture"
+            },
+            {
+                type: "input",
+                message: "Please enter User GitHub email",
+                name: "email"
+            }
+              
+        ]);
+
+        writeFileAsync('README.md',`
+        # ${userInput.title}
+
+        ${userInput.badge}
+        
+        ## Description
+
+        ${userInput.description}
+                
+        '''sh
+        node index.js
+        '''
+
+        ## Contents
+        
+        ${userInput.contents}
+        
+        ## Installation 
+
+        ${userInput.installation}
+        
+        ## Author
+        
+        ${userInput.author}
+        
+        ## Project homepage
+        
+        [GitHub page](${userInput.homepage})
+        
+        ## Usage
+        
+        ${userInput.usage}
+        
+        ## License name
+        
+        ${userInput.licenseName}
+        
+        ## License URL
+        
+        ${userInput.licenseURL}
+        
+        ## Contributing
+        
+        ${userInput.contributing}
+        
+        ## Tests
+        
+        ${userInput.tests}
+        
+        ## GitHub username
+
+        [GitHub profile](https://github.com/${userInput.username})
+        
+        ## User GitHub profile picture
+        
+        ![Picture](${userInput.picture})
+        
+        ## User GitHub email
+
+        ${userInput.email}
+        
+        ## User story:
+        
+        
+        AS A developer
+        
+        I WANT a README generator
+        
+        SO THAT I can easily put together a good README for a new project
+        `
+        );
+    
+    } 
+    catch (err) {
+        console.log(err);
+        }
+}
+
